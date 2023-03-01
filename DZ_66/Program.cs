9f76ad063ff3ef6bfd9046d4c1eb5dec381e3b4f
Elya Megrabyan <m-elia@yandex.ru>
2023-03-02 02:31:03 +0300

    Домашняя задача 66

---
+++ DZ_66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Sum(m, n);
+
+
+void Sum(int m, int n)
+{
+    Console.Write(otvet(m - 1, n));
+}
+
+
+int otvet(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + otvet(m, n);
+        return res;
+    }
+}
